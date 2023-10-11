8afa68936cfd82ad7431420b131f1746a43003e7
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-10-11 23:56:49 +0300

    chore:🌻 first project in Rust

--- common-concepts/src/main.rs
+++ common-concepts/src/main.rs
@@ -1,22 +1,57 @@
 fn main() {
-    let mut total = 0;
-    for fib in 1..7  {
-        total += fibonaci(fib);
-    }
-    println!("{total}");
+    let mut a = String::from("value sami tests");
+    let r1 = &a;
+    let r2 = &a;
+    let r3 = &a;
 
-    let romania = celsius(30.0);
-    println!("{}", romania)
+    let ab = first_word(&a);
+
+    println!("{ab}");
+    let ax = second_first_word(&a);
+    println!("{ax}");
+    println!("{a}");
 }
 
-fn fibonaci (number: i32)-> i32 {
-    if(number <=1){
+fn fibonaci(number: i32) -> i32 {
+    if number <= 1 {
         return 1;
     }
-    return fibonaci(number-1) + fibonaci(number-2);
+    return fibonaci(number - 1) + fibonaci(number - 2);
 }
 // 1 2 3 5 8 13 21
 
-fn celsius(grade:f32) -> f32{
+fn celsius(grade: f32) -> f32 {
     return grade * 1.8 + 32.0;
 }
+
+fn modified(ida: &String) -> usize {
+    ida.len()
+}
+
+fn modified2(ida: &mut String) {
+    ida.push_str("string")
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    s.len()
+}
+
+fn second_first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
