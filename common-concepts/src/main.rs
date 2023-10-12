ee003e43b77c249c7d7d44c0f23655efcbb4e11f
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-10-13 02:33:42 +0300

    chore:🌻 create struct

--- common-concepts/src/main.rs
+++ common-concepts/src/main.rs
@@ -1,56 +1,18 @@
+use std::str::FromStr;
+
 fn main() {
-    let mut a = String::from("value sami tests");
-    let r1 = &a;
-    let r2 = &a;
-    let r3 = &a;
+    let mut andrei = generate_user(String::from("Andrei"),String::from("andrei@gmai.com"));
 
-    // let ab = first_word(&a);
-
-    // prinln!("{ab}");
-    let ax = second_first_word(&a);
-    println!("{ax}");
-    println!("{a}");
+    println!("{:?}", &andrei  )
 }
 
-fn fibonaci(number: i32) -> i32 {
-    if number <= 1 {
-        return 1;
-    }
-
-    return fibonaci(number - 1) + fibonaci(number - 2);
+#[derive(Debug)]
+struct User {
+    name: String,
+    age: i32,
+    email:String
 }
-// 1 2 3 5 8 13 21
 
-// fn celsius(grade: f32) -> f32 {
-//     return grade * 4.8 + 32.0;
-// }
-//
-// fn modified(ida: &String) -> usize {
-//     ida.len()
-// }
-//
-// fn modified2(ida: &mut String) {
-//     ida.push_str("string")
-// }
-//
-// fn first_word(s: &String) -> usize {
-//     let bytes = s.as_bytes();
-//     for (i, &item) in bytes.iter().enumerate() {
-//         if item == b' ' {
-//             return i;
-//         }
-//     }
-//     s.len()
-// }
-
-fn second_first_word(s: &String) -> &str {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..i];
-        }
-    }
-
-    &s[..]
+fn generate_user(name: String, email: String) -> User{
+    User { name, age:0, email }
 }
