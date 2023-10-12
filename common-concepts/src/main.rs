d0e38c272ae78c7c81aea7ed037fb790edc207e2
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-10-13 02:42:22 +0300

    chore:🌻 create some logic for struct

--- common-concepts/src/main.rs
+++ common-concepts/src/main.rs
@@ -1,18 +1,38 @@
 use std::str::FromStr;
 
 fn main() {
-    let mut andrei = generate_user(String::from("Andrei"),String::from("andrei@gmai.com"));
+    let mut andrei = generate_user(String::from("Andrei"), String::from("andrei@gmai.com"));
 
-    println!("{:?}", &andrei  )
+    println!("{:?}", &andrei);
+
+    let verdict_for_notification: &str = send_notificaiton(&andrei);
+    println!("{}", verdict_for_notification)
 }
 
 #[derive(Debug)]
 struct User {
     name: String,
     age: i32,
-    email:String
+    email: String,
 }
 
-fn generate_user(name: String, email: String) -> User{
-    User { name, age:0, email }
+fn generate_user(name: String, email: String) -> User {
+    User {
+        name,
+        age: 0,
+        email,
+    }
+}
+
+fn send_notificaiton(user: &User) -> &str {
+    match user.age > 10 {
+        true => {
+            println!("Notification was sended!");
+            "You are lucky"
+        }
+        false => {
+            println!("No notification was sended!");
+            "You badly"
+        }
+    }
 }
