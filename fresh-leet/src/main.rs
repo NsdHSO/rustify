0cfb62de6dfae1abda5c0688df58d07d560e08b1
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-10-16 09:20:21 +0300

    chore:🌻 add roman number to arabic

--- fresh-leet/src/main.rs
+++ fresh-leet/src/main.rs
@@ -1,23 +1,61 @@
 use std::collections::HashSet;
+use std::cmp;
 
 fn main() {
-
-    length_of_longest_substring(String::from("iuiffca"));
+    let mut roman_string= String::from("LVIIIV");
+    println!("{:?}", convert_roman_in_int(&roman_string));
 }
 
-fn length_of_longest_substring(s: String) -> u8{
-    let mut start: usize = 0;
-    let mut max = 0;
-    let mut map: HashSet<char> = HashSet::new();
-    for c in s.chars() {
-        while map.get(&c).is_some() {
-            map.remove(&s.chars().nth(start).unwrap());
-            start += 1;
+fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+    let mut c_vec: Vec<i32> = [nums1, nums2].concat();
+    c_vec.sort();
+
+    let c_len = (c_vec.len() - 1) >> 1;
+    println!("{:?}", (c_vec.len() - 1 >> 1));
+    (c_vec[c_len] + c_vec[c_len + (c_vec.len() - 1 & 1)]) as f64 / 2.0
+}
+
+
+fn is_palindrome(x: i32) -> bool {
+    let binding = x.to_string();
+    let num_str = binding.as_bytes();
+    let mut max = binding.len() - 1;
+    for _index in 0..num_str.len() {
+        if binding.chars().nth(_index).unwrap() == binding.chars().nth(max).unwrap() {
+            if max > 0 {
+                max -= 1;
+            }
+        } else {
+            return false;
         }
-
-        map.insert(c);
-        max = std::cmp::max(max, map.len());
     }
-    max as u8
+    true
 }
 
+fn is_pal(x: i32) -> bool {
+    x.to_string().chars().rev().eq(x.to_string().chars())
+}
+
+fn convert_roman_in_int(s: &String) -> i32 {
+    let s_translated = s
+        .replace("IV", "IIII")
+        .replace("IX", "VIIII")
+        .replace("XL", "XXXX")
+        .replace("XC", "LXXXX")
+        .replace("CD", "CCCC")
+        .replace("CM", "DCCCC");
+
+    println!("{}",s_translated);
+    s_translated.chars().map(|c| {
+        match c {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => 0,
+        }
+    }).sum()
+}
